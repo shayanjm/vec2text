6611cf82e1090d7d65aafa2a625edfb29bbced0b
shayanjm <shayan.mohanty@gmail.com>
2024-11-05 15:55:50 -0800

    Get rid of shared params

--- vec2text/trainers/inversion.py
+++ vec2text/trainers/inversion.py
@@ -30,7 +30,7 @@ class InversionTrainer(BaseTrainer):
         self.gradnorm = GradNormLossWeighter(
             num_losses=2,  # Number of tasks
             alpha=0.5,     # Hyperparameter for restoring force
-            grad_norm_parameters=list(self.model.parameters()),  # Shared parameters
+            grad_norm_parameters=None,  # Shared parameters
             device=self.args.device  # Ensure GradNorm operates on the correct device
         )
 
