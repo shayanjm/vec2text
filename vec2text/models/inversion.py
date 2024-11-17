9b4f8498b4f70c6cc6b1401dde057fb0aff23342
shayanjm <shayan.mohanty@gmail.com>
2024-11-17 18:58:53 +1100

    oops x2

--- vec2text/models/inversion.py
+++ vec2text/models/inversion.py
@@ -121,8 +121,9 @@ class InversionModel(transformers.PreTrainedModel):
         self.embeddings_from_layer_n = embeddings_from_layer_n
         self.noise_level = vars(config).get("embedder_gaussian_noise_level")
 
-        self.log_sigma_cos = nn.Parameter(torch.tensor(0.0))
-        self.log_sigma_mse = nn.Parameter(torch.tensor(0.0))
+        self.log_sigma_ce = nn.Parameter(torch.tensor(0.0))
+        self.log_sigma_cosine_embedding = nn.Parameter(torch.tensor(0.0))
+        self.log_sigma_mse_embedding = nn.Parameter(torch.tensor(0.0))
 
     def _freeze_encoder(self):
         freeze_params(self.encoder_decoder.encoder)
