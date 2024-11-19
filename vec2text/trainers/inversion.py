c15cb6f35613b3977bab4a5d7d4f959fd175d044
shayanjm <shayan.mohanty@gmail.com>
2024-11-19 14:38:23 +1100

    Trying another thing

--- vec2text/trainers/inversion.py
+++ vec2text/trainers/inversion.py
@@ -72,10 +72,7 @@ class InversionTrainer(BaseTrainer):
 
         # Use the generate method to get the model's predictions
         generation_kwargs = {
-            'max_length': self.model.config.max_seq_length,
             'num_beams': 1, 
-            'do_sample': False,  # Disable sampling
-            'early_stopping': True,
             'use_cache': False,
         }
 
